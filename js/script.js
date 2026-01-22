46c44256a7bc828f7a4574c796d09b21667c6389
Joel Moh <heitor.juliani19@gmail.com>
2026-01-21 18:52:11 -0700

    version 0.3.0 - theme added

--- js/script.js
+++ js/script.js
@@ -50,3 +50,32 @@ async function getRepos() {
 }
 
 getRepos();
+
+
+
+// theme-toggle button
+
+const themeToggleBtn = document.querySelector('.theme-toggle');
+const currentTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    if (currentTheme === "dark") {
+        themeToggleBtn.innerHTML = '<img src="/img/moon.svg" alt="">';
+    } else {
+        themeToggleBtn.innerHTML = '<img src="/img/sun.svg" alt="">';
+    }
+}
+
+themeToggleBtn.addEventListener('click', function() {
+    let theme = document.documentElement.getAttribute("data-theme");
+    if (theme === "light") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+        themeToggleBtn.innerHTML = '<img src="/img/moon.svg" alt="">';
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+        themeToggleBtn.innerHTML = '<img src="/img/sun.svg" alt="">';
+    }
+});
